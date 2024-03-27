98628ac32d94b0485af81be7464eaed1aec3bb38
kenless1 <48138341+kenless1@users.noreply.github.com>
2024-03-27 15:02:28 -0400

    Update Calculateur.java

--- Calculateur.java
+++ Calculateur.java
@@ -1,3 +1,20 @@
+/**
+ * L'interface Calculateur définit les opérations pour calculer une valeur basée sur un nombre donné et obtenir un compteur interne.
+ */
 public interface Calculateur {
-    void calculer(long N);
+    
+    /**
+     * Obtient la valeur actuelle du compteur interne.
+     * 
+     * @return La valeur actuelle du compteur.
+     */
+    public long obtenirCompteur();
+
+    /**
+     * Calcule une nouvelle valeur basée sur le nombre N fourni.
+     * 
+     * @param N Le nombre sur lequel effectuer le calcul.
+     * @return Le résultat du calcul.
+     */
+    long calculer(long N);
 }
